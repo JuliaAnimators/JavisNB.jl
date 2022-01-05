53040510931e65925f38c8850e1d6a13a0f365a3
gpucce <g.puccetti92@gmail.com>
2022-01-05 20:23:18 +0100

    improve tests

--- test/test_pluto_viewer.jl
+++ test/test_pluto_viewer.jl
@@ -7,8 +7,8 @@ using InteractiveUtils
 # ╔═╡ e7cac400-6751-11ec-3910-af3121135a31
 begin
     using Pkg
-    Pkg.activate(".")
-    using Revise, JavisNB, Javis, PlutoTest
+    Pkg.activate("..")
+    using Revise, JavisNB, Javis
 end
 
 # ╔═╡ e7cac400-6751-11ec-3498-bd690b38b180
@@ -24,14 +24,36 @@ let
     Background(1:100, ground("white", "black"))
     ob = Object(JCircle(O, 30, color = "black", action = :fill))
     act!(ob, Action(1:50, anim_translate(Point(100, 0))))
-    act!(ob, Action(50:100, anim_translate(Point(-100, 0))))
-    v = render(myvideo, pathname = "files/test_pluto_viewer.gif")
+    act!(ob, Action(51:100, anim_translate(Point(-100, 0))))
+    v = render(myvideo, pathname = "temp_files/test_pluto_viewer_render.gif")
 end
 
+# ╔═╡ 9dc6164c-7896-4114-acad-2229a3b93b7a
+embed("temp_files/test_pluto_viewer_render.gif")
+
 # ╔═╡ 50f9030b-ac9e-4d56-99f1-fb6fe721f1ac
-embed("files/test_pluto_viewer.gif")
+let
+    function ground(c1, c2)
+        (args...) -> begin
+            background("white")
+            sethue("black")
+        end
+    end
+
+    myvideo = Video(500, 500)
+    Background(1:100, ground("white", "black"))
+    ob = Object(JCircle(O, 30, color = "black", action = :fill))
+    act!(ob, Action(1:50, anim_translate(Point(100, 0))))
+    act!(ob, Action(51:100, anim_translate(Point(-100, 0))))
+    embed(myvideo, tempdirectory = "temp_files", pathname = "")
+end
+
+# ╔═╡ 109494be-9b41-4c8b-9f6c-26ffc361fa12
+nothing
 
 # ╔═╡ Cell order:
 # ╠═e7cac400-6751-11ec-3910-af3121135a31
 # ╠═e7cac400-6751-11ec-3498-bd690b38b180
+# ╠═9dc6164c-7896-4114-acad-2229a3b93b7a
 # ╠═50f9030b-ac9e-4d56-99f1-fb6fe721f1ac
+# ╠═109494be-9b41-4c8b-9f6c-26ffc361fa12
